1f4c4d877b9320d5440633f9d57b44dee70c2972
Chenchao Zhao <chenchao.zhao@clarifai.com>
2022-07-19 11:42:19 -0400

    addSegUploader

--- src/engine/image.py
+++ src/engine/image.py
@@ -64,3 +64,36 @@ class ImageClassification(_EngineBase):
         concepts = [transform.label_to_concept_proto(l) for l in labels]
 
         return transform.to_input(image=image, concepts=concepts)
+
+
+class ImageSegmentation(_EngineBase):
+    """
+    Image Segmentation Data Uploader.
+
+    __init__:
+        Args:
+            channel (str): tag in `dev`, `staging`, `prod` or custom endpoint
+            api_key (str): app api key
+            batch_size (int, optional): batch size of inputs. Defaults to MAX_BATCH_SIZE.
+            max_num_of_trials (int, optional): max number of trials. Defaults to 100.
+    """
+
+    def to_proto(
+        self, image: Image, labels: List[str], binary_maskes: List[Image]
+    ) -> transform.data.resources_pb2.Input:
+        """
+        to_proto.
+
+        Args:
+            image (Image): PIL Image object
+            labels (List[str]): list of string labels
+            binary_maskes (List[Image]): list of black-white PNG (mode=`1`)
+
+        Returns:
+            resources_pb2.Input: input proto
+        """
+
+        image = transform.pil_to_proto(image)
+        # region = todo region contains the concepts and maskes
+
+        return transform.to_input(image=image)
